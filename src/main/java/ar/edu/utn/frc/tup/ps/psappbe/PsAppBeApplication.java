45b35cf39b017ff9eb65031c9ebd403d864305a7
Hernán Jesús Morais <104473@tecnicatura.frc.utn.edu.ar>
2022-10-08 15:17:17 -0300

    Initial scaffolding spring boot

---
+++ src/main/java/ar/edu/utn/frc/tup/ps/psappbe/PsAppBeApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.utn.frc.tup.ps.psappbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsAppBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsAppBeApplication.class, args);
+	}
+
+}
