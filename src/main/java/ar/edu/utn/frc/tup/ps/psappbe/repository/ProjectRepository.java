85d75df8976795dfc78f68d41c2b159f5fdc0ab0
14473HJM <104473@tecnicatura.frc.utn.edu.ar>
2022-12-11 12:44:13 -0300

    Update ProjectRepository.java

--- src/main/java/ar/edu/utn/frc/tup/ps/psappbe/repository/ProjectRepository.java
+++ src/main/java/ar/edu/utn/frc/tup/ps/psappbe/repository/ProjectRepository.java
@@ -1,6 +1,7 @@
 package ar.edu.utn.frc.tup.ps.psappbe.repository;
 
 import ar.edu.utn.frc.tup.ps.psappbe.domain.people.Professor;
+import ar.edu.utn.frc.tup.ps.psappbe.entities.people.ProfessorEntity;
 import ar.edu.utn.frc.tup.ps.psappbe.entities.project.ProjectEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -16,6 +17,6 @@ public interface ProjectRepository extends JpaRepository<ProjectEntity, Long> {
 
     @Query("SELECT P " +
             "FROM ProjectEntity P " +
-            "WHERE P.tutor = :tutorId")
+            "WHERE P.tutor.id = :tutorId")
     List<ProjectEntity> getAllByTutor(Long tutorId);
 }
